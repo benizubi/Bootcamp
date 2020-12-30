71abeccf1f7ff32c2ad4fad5616d9c20a2a3a022
benizubi <benizubi@outlook.com>
2020-12-30 01:05:56 +0000

    Completed Ifs examples

--- Bootcamp/5. Javascript/1.If Statements/script.js
+++ Bootcamp/5. Javascript/1.If Statements/script.js
@@ -11,19 +11,19 @@
 //     console.log("YOUR NUMBER IS Greater THAN 0.5")
 //     console.log(random);
 // }
-const dayOfWeek = prompt().toLowerCase();
-// the lowercase has been set so it matches & prompt brings out an input field
+// const dayOfWeek = prompt().toLowerCase();
+// // the lowercase has been set so it matches & prompt brings out an input field
 
 
-if (dayOfWeek === 'monday') {
-    console.log("UGH I hate Mondays")
-} else if (dayOfWeek === 'saturday') {
-    console.log("YAY I LOVE SATURDAYS!")
-} else if (dayOfWeek === 'friday') {
-    console.log("Fridays r okay, especially after work")
-} else {
-    console.log("MEH")
-}
+// if (dayOfWeek === 'monday') {
+//     console.log("UGH I hate Mondays")
+// } else if (dayOfWeek === 'saturday') {
+//     console.log("YAY I LOVE SATURDAYS!")
+// } else if (dayOfWeek === 'friday') {
+//     console.log("Fridays r okay, especially after work")
+// } else {
+//     console.log("MEH")
+// }
 // remember the else always check for anything else that's not already included in our if statement, will print only if not met any others before that line
 
 // 0-5 - free
@@ -42,3 +42,30 @@ if (dayOfWeek === 'monday') {
 //     console.log("Do whatever you like")
 // }
 // // can have as many else if as you want.
+
+
+const password = prompt("please enter a new password");
+
+// the password must be 6+ characters: 
+if (password.length >= 6) {
+    // here below wenested both if statements together so to display the right log based on the issue ,
+    //  the code below commented out would work but can only print 1 log at a time
+    if (password.indexOf('') == -1) {
+        // indexOf is used to check for what number is the characters and ' ' for spaces between charaters it's -1 
+        // so to check for space we check for -1
+        console.log("Valid password!")
+    }else{
+        console.log("Password cannot contain spaces!")
+    } 
+} else {
+    console.log("PASSWORD TOO SHORT! Must be 6+ characters")
+
+}
+// password cannot include spaces:
+// if (password.indexOf('') == -1) {
+//     // indexOf is used to check for what number is the characters and ' ' for spaces between charaters it's -1 
+//     // so to check for space we check for -1
+//     console.log("Good Job! No space!")
+// }else{
+//     console.log("Password cannot contain spaces!")
+// } 
