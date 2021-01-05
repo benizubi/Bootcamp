3c92774a2a23fa795536f846ab6f626f03069d4b
benizubi <benizubi@outlook.com>
2021-01-05 23:42:09 +0000

    Repeating Stuff With Loops

---
+++ Bootcamp/5. Javascript/5.Repeating Stuff with Loops/app.js
@@ -0,0 +1,37 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+// For Loop here is doing the same as what's listed above
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+// Even numbers 0-20
+for (let a = 0; a <= 20; a += 2) {
+
+}
+// so for the even number loop 0 is where we wanna strt, if i wanted to start at 4, i'd change the 0 to 4 for example
+// Also the == and += is the same thing when using a loop.  the 2 at the end here is that so it returns even number which is 2
+// so for it to be an even number it adds the 0 and 2 which gives 2. 
+
+// Odd number 1-20
+for (let e = 1; e <= 20; e += 2) {
+    console.log(e)
+}
+// for odd number we'd have written 1 instead of 0 and  leave 2 because 2+1 gives u 3 and that's an odd number
+
+// To count from 100-0 printing in 10s
+for (let c = 100; c >= 0; c -= 10) {
+    console.log(i);
+}
+// here we have to subtract 10 each time from c  and c is greater or same as 0
+// or could even do it like this so it prints  10 as the first, then it * by 10 each time and asnwr <=1000.
+for (let b = 10; b <= 1000; b *= 10) {
+    console.log(b);
+}
