accdeb0448bcb75663dd07224d69948466f2a8e7
benizubi <benizubi@outlook.com>
2021-01-11 23:34:13 +0000

    Done Function

--- Bootcamp/5. Javascript/7. Functions/apps.js
+++ Bootcamp/5. Javascript/7. Functions/apps.js
@@ -1,4 +1,4 @@
-// Functions // making your codes reuseable and written nicely
+// Functions // making your codes reuseable and written nicely // every methods is a function //
 
 let die1 = Math.floor(Math.random() * 6) + 1;
 let die1 = Math.floor(Math.random() * 6) + 1;
@@ -18,4 +18,39 @@ groupies();
 // so an order to run this function we have to call it like this an order for it to actully run
 
 
-// Arguments // an Argument is simply the values being passed on 
+// Arguments // an Argument is simply the values being passed on
+// Arguments refers to the values being in here for example greet ('tim') so tim is the argument
+// indeoOf('h'); the h will be the argument
+// even the console.log(), we're still passing on an argument to be executed
+// here's how we pass the arguments
+function greeting(firstname) {
+    // so here firstname is just the parameter, but what we type in firstname when calling it is called the argument
+    // there's no logic or deepeer meaning of the argument being firstname, as javascript doesn't know it, just for my own sake
+    console.log(`Firstname is: ${firstname}`);
+}
+greeting('hi')
+// also when calling the function if the argument is not passed on the its going to give undefined.
+
+// Functions with Multiple Arguments // we can have more than 2 arguments 
+
+// Make sure everything has to follow the same order throughout as everything is decided by order
+function names(firstnames, lastname) {
+    console.log(`Hey there, ${firstnames} ${lastname[0]}.`)
+    // here I want the first name and first initial of last name
+}
+names('beni', 'zubi')
+// This will return Hey there, Beni Z. in that order first and last name
+
+function repeat(str, numTimes) {
+    // we can also ignore an argument, but it could cause an error only if retrying to do something and not working because we ignored the argument
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        // console.log("Hi");
+        result += str;
+    }
+}
+repeat('fsfddfs', 5)
+// so if I called this, it will print out 'Hi' 5 times because of the loop and the second argument is asking for the numbers of time being repeated
+
+repeat('chicken', 10)
+// because its a loop it will print it 10 times as we've asked on the second paremeter
