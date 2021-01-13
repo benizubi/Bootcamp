301d96fac6ef9cb259b3de9889f63441f11b0e20
benizubi <benizubi@outlook.com>
2021-01-13 08:20:24 +0000

    Completed Functions

--- Bootcamp/5. Javascript/8. Levelling Up Functions/apps.js
+++ Bootcamp/5. Javascript/8. Levelling Up Functions/apps.js
@@ -73,7 +73,108 @@ const square = function (n) {
     let a = n * n;
     return a;
 };
-
 // Higher Order Functions //
 // Functions that operate on with other functions.
 // They can accept other functions as arguments and return a function
+function callTwice(func) {
+
+    func();
+    func()
+}
+function laugh() {
+    console.log("beni")
+}
+callTwice(laugh)
+// so basically the first function is used to store a prepetion of twice and then second function simple prints  and can be used to gether to print the values like done above
+// here function will print beni twice and if i wanted 10 timex? i can just screate a for loop repeating 10 times and link them together
+
+// Returning Functions //
+
+function makeMysteryFunc() {
+    const rand = Math.random();
+    if (rand > 0.5) {
+        return function () {
+            console.log('congrats');
+            console.log('congrats');
+            console.log('congrats');
+        }
+    } else {
+        return function () {
+            alert("FAILED ")
+            alert("FAILED ")
+            alert("FAILED ")
+        }
+    }
+}
+// You can return functions like this.
+// Another example 
+function makeBetweenFunc(min, max) {
+    return function (num) {
+        return num >= min && num <= max;
+    }
+}
+makeBetweenFunc(100, 200)
+// so we can create more like const isChild = makeBetweenFunc(0,18) and that will only return the type of values selected.
+// so this functino returns the min and max value and we can set different const names and values to return to us true or false and if they meet that age
+
+
+// Defining Methods //
+// a Method is a function that's been placed as a property on a object
+// We can add functions as properties on objects. we call them methods 
+const myMath = {
+    PI: 3.2,
+    // square: function (num) {
+    square(num) {
+        return num * num;
+    },
+    // cube: function (num) { this one and bellow is the same you can write functions, but u dont need it and can write like this and get the same result.
+    cube(num) {
+        return num ** 3;
+    }
+}
+Math.cube(2);
+
+// THIS in Methods //
+// Use the eyword this to access other properties on the same object.
+const cat = {
+    name: 'beni',
+    color: 'black',
+    breed: 'pyjama squid',
+    meow() {
+        console.log(`${this.name} says MEOWWWWW`);
+    }
+    // usually in a object i can use console log to print out everything in cat, but if i tried to print a key it will return an error 
+    // so if i wanted to be specific that's where this comes in display like used above
+}
+cat.meow();
+const meow2 = cat.meow;
+
+meow2();
+// The value of this depends n the invocation context of the funtion it is used in.
+// so the this methods works by focus on the value we have placed on the left of the . so here its cat for cat.meow(), so it returns the ojbect function as desired
+
+// so if i call meow2(), it will not give back the same value like it did on the first time because this time when we call it its just one word and there's no . so by default the this stores it on the window and that looks more like 
+// window.meow2(), even tho the meow2 is not written, but that's where it will be stored.
+// so using this will evalluate  based on how you write ur code
+// another example // this increases the eggcount by 1 + each time ++
+const hen = {
+    name: 'Helen',
+    eggCount: 0,
+    layAnEgg: function () {
+        let a = 'EGG';
+        this.eggCount;
+        return a;
+    }
+};
+
+// Try/Catch //
+// the try and catch both has to be called for it to work and you try something and and if it doesnt run u return something to catch e.g
+
+// checking to see if its a string if not return this
+function yell(mg){
+    try{
+        console.log(mg.toUpperCase().repeat(3));
+    }catch{
+        console.log("Please Enter a String only")
+    }
+}
