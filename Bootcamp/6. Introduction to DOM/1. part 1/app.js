46aab9a303094e687c1f258b98d101f3cb54adef
benizubi <benizubi@outlook.com>
2021-01-17 23:44:17 +0000

    DOM updated

--- Bootcamp/6. Introduction to DOM/1. part 1/app.js
+++ Bootcamp/6. Introduction to DOM/1. part 1/app.js
@@ -3,3 +3,16 @@
 // for(let img of images){
 //     console.log(img.src)
 // }
+const allLinks = document.querySelectorAll('a');
+
+// for(let link of allLinks){
+//     link.innerText= ' I am a link'
+//     // by doing this we are overriding what's been set through the HTML
+// }
+
+// for(let links of allLinks){
+//     links.style.color = 'grb(0,108,134)';
+// allLinks.style.textDecorationColor ='blue';
+// allLinks.style.textDecorationStyle ='unerlined';
+
+// } changed here all links color
