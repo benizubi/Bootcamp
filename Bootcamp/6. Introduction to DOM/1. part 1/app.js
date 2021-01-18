7ff6e1d4232f0bcf0ef6fb9560cf216882f16a3f
benizubi <benizubi@outlook.com>
2021-01-18 23:44:26 +0000

    Second Dom

--- Bootcamp/6. Introduction to DOM/1. part 1/app.js
+++ Bootcamp/6. Introduction to DOM/1. part 1/app.js
@@ -16,3 +16,12 @@ const allLinks = document.querySelectorAll('a');
 // allLinks.style.textDecorationStyle ='unerlined';
 
 // } changed here all links color
+
+
+// document.querySelectorAll('span')[0].style.color = colors[0];
+// document.querySelectorAll('span')[1].style.color = colors[1];
+// document.querySelectorAll('span')[2].style.color = colors[2];
+// document.querySelectorAll('span')[3].style.color = colors[3];
+// document.querySelectorAll('span')[4].style.color = colors[4];
+// document.querySelectorAll('span')[5].style.color = colors[5];
+// document.querySelectorAll('span')[6].style.color = colors[6];
