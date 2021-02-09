ed8fa155230e5e3004ae2d62b49458257d30f211
benizubi <benizubi@outlook.com>
2021-02-09 00:02:40 +0000

    Completed Fetch1

--- Bootcamp/8. AJAX & API's/3. Fetch API/app.js
+++ Bootcamp/8. AJAX & API's/3. Fetch API/app.js
@@ -3,7 +3,13 @@
 
 fetch('https://api.cryptonator.com/api/full/btc-usd')
     .then(res => {
-        console.log("RESPONSE", res)
+        console.log("RESPONSE, waiing to parce", res)
+        return res.json();
+        // the .json basically returns a promise as json format type 
+    })
+    .then(data => {
+        console.log("DATA PARSED...", data)
+
     })
     .catch(e => {
         console.log("ERROR", e)
