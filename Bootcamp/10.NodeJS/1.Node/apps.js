5cd121e6575e4c9e93cb1e703ed8e11b403a552d
benizubi <benizubi@outlook.com>
2021-02-12 11:34:03 +0000

    Intro to node.js

---
+++ Bootcamp/10.NodeJS/1.Node/apps.js
@@ -0,0 +1,9 @@
+// Node//
+// it's a javascript runtime, so it's an implementation of javascript that runs outside of the browsers, for e.g like on the server side of things
+// same commands we use on js can be used there too
+// Node is used to make web servers, commands lines tools, (vscode is a node app!), video games, drone software and many more.
+const args = process.argv.slice(2);
+for (let arg of args) {
+    console.log(`Hi there,${arg}`)
+    // used slide so we can ignore the first two lines of info reguarding the file and the next line will return this if we run 
+}
