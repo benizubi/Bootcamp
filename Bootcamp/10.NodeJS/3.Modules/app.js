5247ba948818fae125019f3561d0c13ca6858fd6
benizubi <benizubi@outlook.com>
2021-02-13 00:18:15 +0000

    Modules and exports done

--- Bootcamp/10.NodeJS/3.Modules/app.js
+++ Bootcamp/10.NodeJS/3.Modules/app.js
@@ -1,3 +1,10 @@
 const math = require('./Math');
 // when reference a page we have to add the . so it adds it to this specific directory 
-console.log(math)
+// here we've connecting the math page so it needs the key word require
+console.log(math.PI);
+console.log(math.square(9))
+// Can either call it this way or the other method way if i only wanted to access pi and square i can call it in an object then reference it
+ const {PI, square} = require('./Math');
+console.log(square(9))
+console.log(PI)
+
