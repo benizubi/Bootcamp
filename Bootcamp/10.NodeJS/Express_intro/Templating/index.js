2f2ec262da47f285621f57de9786c385e508a4e2
benizubi <benizubi@outlook.com>
2021-02-17 22:07:32 +0000

    Getting Views Directory using path

--- Bootcamp/10.NodeJS/Express_intro/Templating/index.js
+++ Bootcamp/10.NodeJS/Express_intro/Templating/index.js
@@ -1,6 +1,8 @@
 // Express 
 const express = require('express');
 const app = express();
+const path = require('path');
+// the path
 
 
 app.set('view engine', 'ejs');
@@ -9,8 +11,15 @@ app.set('view engine', 'ejs');
 // also downloaded the view folder with the ejs file
 
 app.get('/', (request, response) => {
-    response.send("HI")
-
+    response.render('home.ejs')
+    // instead of the send method, we're using rendor here and it allows us to atach a ejs file to read on the browser
+    // i dont have to list .ejs here before we've already listed ejs on the view engine.
+    // an order for the ejs file to run, on the terminal you can only run it from the correct directory so that the it can detect the views folder 
+    // otherwise it will return error message on the browser, although the server will still run
+    // in order to run it outside of the correct directory, we can use path 
+    app.set('views', path.join(__dirname, '/views'))
+    // path.join takes multiple segments and joins them together into a single path 
+    // instead of running the code from the right directory, here we say instead run the file from where the index.js file directory is instead 
 })
 
 
