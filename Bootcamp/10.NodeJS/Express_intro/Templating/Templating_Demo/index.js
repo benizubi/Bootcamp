6fc1a6ce605040619fcbc637f7a3ce5f11da2a05
benizubi <benizubi@outlook.com>
2021-02-18 23:53:27 +0000

    subrreddit Demo

--- Bootcamp/10.NodeJS/Express_intro/Templating/Templating_Demo/index.js
+++ Bootcamp/10.NodeJS/Express_intro/Templating/Templating_Demo/index.js
@@ -3,6 +3,7 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
 
 
 app.set('view engine', 'ejs');
