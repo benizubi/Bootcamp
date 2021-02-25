3039a5c9ea31643b52962798cd27b4c19a960053
benizubi <benizubi@outlook.com>
2021-02-25 21:19:34 +0000

    sucessfully connected mongoose

--- Bootcamp/10.NodeJS/Mongoose/MongooseBasics/index.js
+++ Bootcamp/10.NodeJS/Mongoose/MongooseBasics/index.js
@@ -1,11 +1,11 @@
-// we're connecting  to mongoDB and the default ports are 27017, 27018,27019
+// we're connecting  to mongoDB and the default ports are 27017, 27018,27019 there's only up to 6 ports
 const mongoose = require('mongoose');
 // we're defining which database to use, here we're using movieApp database and if it doesnt exist, it will create one, or it will use it if it already exists.
-mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// This prints this error when there's an error//
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    // we're connected!
-});
+mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connection Open!")
+    })
+    .catch(err => {
+        console.log(log("OHno error!!!"))
+        console.log(err)
+    })
