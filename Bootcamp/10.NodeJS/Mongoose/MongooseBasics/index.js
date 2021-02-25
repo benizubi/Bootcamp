576485f20e1875c31062c9bf2c831390ffb3e72f
benizubi <benizubi@outlook.com>
2021-02-25 23:27:28 +0000

    fully connected

--- Bootcamp/10.NodeJS/Mongoose/MongooseBasics/index.js
+++ Bootcamp/10.NodeJS/Mongoose/MongooseBasics/index.js
@@ -9,3 +9,20 @@ mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, 
         console.log(log("OHno error!!!"))
         console.log(err)
     })
+// Schema is blueprint of  a mapping of different collections keys in mongoose to different types of js
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+})
+// mongoose will take the movie part, it should always be capitalised and one word, then mangoose will create a collection for us called 'movies' changing it to lower case and plural.
+// we usually call it the same name when saving it to a variable.
+
+const Movie = mongoose.model('Movie', movieSchema);
+// now  after we've have created a collection called movies, then we can use movie to add things into our databse, like down below by calling new movies and passing on data.
+// so amadeus is accessible through node and it will return the values in this, however it's not yet saved to mongodb 
+// to save to Amadeus, in node you will have to call Amadeous.save() and that will store it in the db
+// then 'use movieApp ' to switch to db movieapp and 
+const Amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' })
