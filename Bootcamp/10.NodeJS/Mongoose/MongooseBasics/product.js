5bdf5d8db781fe202e150d203668da3aa995f9a0
benizubi <benizubi@outlook.com>
2021-03-05 23:42:02 +0000

    Additional Schema Constrains

--- Bootcamp/10.NodeJS/Mongoose/MongooseBasics/product.js
+++ Bootcamp/10.NodeJS/Mongoose/MongooseBasics/product.js
@@ -2,13 +2,44 @@
 // we're connecting  to mongoDB and the default ports are 27017, 27018,27019 there's only up to 6 ports
 const mongoose = require('mongoose');
 // we're defining which database to use, here we're using movieApp database and if it doesnt exist, it will create one, or it will use it if it already exists.
-mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    // shopApp is the name of the database we're going to use   
     .then(() => {
         console.log("Connection Open!")
+        // usually our code can go here, but we don't need to nest all our code here 
+    })
+    .catch(err => {
+        console.log(log("OHno error!!!"))
+        console.log(err)
+    })
+// Mongoose Schema Validtions //
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+    // The other schema methods automatically guesses the type because we ddin't set it, but nested objects here allow us to do such tasks. 
+    // by doing this methods it creates a validation method because if i was to run this file without a name given, it will return an error because a name is required
+    // Also if i use a string instead of number, it will also return an error because its requiring to have a number as it failed to retrive such info.
+    // if we were to add on more new things that's not been set up here, it will not return an error, but it will  not store it in the database
+
+})
+const Product = mongoose.model('Product', productSchema);
+
+const bike = new Product(
+    { price: 599 });
+bike.save()
+    // this stores it to the mongo db by calling save
+    .then(() => {
+        console.log("It worked!")
+        // usually our code can go here, but we don't need to nest all our code here 
     })
     .catch(err => {
         console.log(log("OHno error!!!"))
         console.log(err)
     })
 
-    // Mongoose Schema Validtions //
