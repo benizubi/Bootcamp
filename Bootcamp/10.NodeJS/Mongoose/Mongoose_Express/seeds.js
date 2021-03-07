5ef1efa417cf17965c052935ee815e39bd56e465
benizubi <benizubi@outlook.com>
2021-03-07 22:25:26 +0000

    Creating our Model

--- Bootcamp/10.NodeJS/Mongoose/Mongoose_Express/seeds.js
+++ Bootcamp/10.NodeJS/Mongoose/Mongoose_Express/seeds.js
@@ -1,9 +1,5 @@
-const express = require('express');
-const app = express();
-const path = require('path');
 const mongoose = require('mongoose');
 
-// here we're requring our product from the model folder
 const Product = require('./models/product')
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,16 +12,16 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
         console.log(log("OH no error!!!"))
         console.log(err)
     })
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-
-
-app.get('/dog', (req, res) => {
-    res.send('WOOF');
-})
-
-app.listen(3000, () => {
-    console.log("App is listening on port 3000")
-})
+    // created our first instance to store in the db
+// const p = new Product({
+//     name: 'Ruby Grapefruit',
+//     price: 1.99,
+//     category: 'fruit'
+// })
+// p.save().then(p => {
+//     console.log(p)
+// })
+//     .catch(e => {
+//         console.log(e)
+//         // to run the error. 
+//     })
