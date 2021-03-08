3f5a6b9d5fbe9a33e210437fb6e8bccb8347ebfe
benizubi <benizubi@outlook.com>
2021-03-08 18:53:20 +0000

    Products details fixed

--- Bootcamp/10.NodeJS/Mongoose/Mongoose_Express/index.js
+++ Bootcamp/10.NodeJS/Mongoose/Mongoose_Express/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 
 // here we're requring our product from the model folder
 const Product = require('./models/product')
+// the product here is what we're going to use everytime we want to list a product.name, product.id or anything. 
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     // shopApp is the name of the database we're going to use   
@@ -29,6 +30,16 @@ app.get('/products', async (req, res) => {
     res.render('products/index', { products })
     // here the second argument is where we pass the product object
     // This is where all the product will be listed. When using methods like find, we use async and await the product, then we call it back again after. like we've called it back on the second argument to pass all products.
+})
+
+app.get('/products/:id', async (req, res) => {
+    // req.params is requesting the params for this id
+    const { id } = req.params;
+    // to find the id we're using findbyid
+    const foundProductId = await Product.findById(id);
+    res.render('products/show', { foundProductId })
+    // foundProductId is what we will use on the show.ejs page to print out individual things we're enquiring such as name, category and price. 
+    // thus, the const name we set on here is then used to find things by id to allocate what we're after. 
 })
 
 app.listen(3000, () => {
