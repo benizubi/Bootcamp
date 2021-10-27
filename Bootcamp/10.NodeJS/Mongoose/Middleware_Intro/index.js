2c9342553fcc38fb01d45dca3ac26bc44f3f6111
benizubi <benizubi@outlook.com>
2021-10-27 23:32:00 +0100

    manipulating different routes

--- Bootcamp/10.NodeJS/Mongoose/Middleware_Intro/index.js
+++ Bootcamp/10.NodeJS/Mongoose/Middleware_Intro/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+
 app.get('/', (req, res) => {
     res.send('Home Page!')
 })
+app.get('/dogs', (req, res) => {
+    res.send('woof wood!')
+})
+
 app.listen(3000, () => {
     console.log('App is running on localhost:3000')
 })
