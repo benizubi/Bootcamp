4b7fe99cb3f908c691a8d4437f2f4fc0f69e65ce
benizubi <benizubi@outlook.com>
2021-10-27 23:30:08 +0100

    creating route for home page

--- Bootcamp/10.NodeJS/Mongoose/Middleware_Intro/index.js
+++ Bootcamp/10.NodeJS/Mongoose/Middleware_Intro/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-
+app.get('/', (req, res) => {
+    res.send('Home Page!')
+})
 app.listen(3000, () => {
     console.log('App is running on localhost:3000')
 })
