c8103f1efc02606a173a2b60ab53d2ee03774a43
benizubi <benizubi@outlook.com>
2021-10-31 23:54:31 +0000

    'hey' will run on every request we call

--- Bootcamp/10.NodeJS/Mongoose/Middleware_Intro/index.js
+++ Bootcamp/10.NodeJS/Mongoose/Middleware_Intro/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-morgan('tiny')
+// morgan('tiny')
 app.use(() => {
     console.log("HEYYYYY!!!")
 })
