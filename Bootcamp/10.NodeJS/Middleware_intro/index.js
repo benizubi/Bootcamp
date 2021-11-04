cd326d8afee3d982dbedaec884fadec209cef274
benizubi <benizubi@outlook.com>
2021-11-04 22:48:27 +0000

    First middleware

--- Bootcamp/10.NodeJS/Middleware_intro/index.js
+++ Bootcamp/10.NodeJS/Middleware_intro/index.js
@@ -3,10 +3,17 @@ const app = express();
 const morgan = require('morgan');
 
 
-// app.use(morgan('dev'))
 // when using morgan to pass informtation to the server, it returns a log of the information we're passing and printing it on the server.
 // .use is telling the app to use function on the server everytime we make a request
+// when using morgan everytime you make a request, it records the log and moves onto the next request u make
+// morgan is a useful login tool.
 
+app.use(morgan('dev'))
+app.use((req, res, next) => {
+    console.log("This is my first middleware!!!")
+    next();
+
+})
 
 
 
