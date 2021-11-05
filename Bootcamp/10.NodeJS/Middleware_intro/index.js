bf5ab0ef919a7c9d37866212889a940e11933929
benizubi <benizubi@outlook.com>
2021-11-05 22:40:05 +0000

    return next() to ensure nothing happens after that

--- Bootcamp/10.NodeJS/Middleware_intro/index.js
+++ Bootcamp/10.NodeJS/Middleware_intro/index.js
@@ -8,23 +8,29 @@ const morgan = require('morgan');
 // when using morgan everytime you make a request, it records the log and moves onto the next request u make
 // morgan is a useful login tool.
 
-app.use(morgan('dev'))
+app.use(morgan('common'))
+
 app.use((req, res, next) => {
     console.log("This is my first middleware!!!")
     // We need to use next so that it moves on to the second middleware how it does with morgan,
     // if i remove next() it will log on the server but doesnt move onto the next call 
-    next();
+    return next();
     // so after calling next, it's going to move onto the next middleware and will only log this after going through all middleware.
+    // return next(); just makes sure nothing happens after next();
     console.log("This is my first middleware after calling next")
 
 
 })
 app.use((req, res, next) => {
     console.log("This is my second middleware!!!")
-    next();
+    return next();
 
 })
+app.use((req, res, next) => {
+    console.log("This is my third middleware!!!")
+    return next();
 
+})
 
 app.get('/', (req, res) => {
     res.send('Home Page!')
